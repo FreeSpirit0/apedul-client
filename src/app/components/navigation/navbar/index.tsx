5d35bc4be2756313cdfe56cac11ca4d6f9f5284e
Poomtum Rattanarat <poomtum2544@hotmail.com>
2023-10-18 20:33:47 +0700

    Add space in navbar

--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React from "react";
 import Link from "next/link";
@@ -11,28 +11,21 @@ const Navbar = () => {
       <div className="w-full h-20 bg-white border-b-2 border-black">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
-            <ul className="hidden md:flex gap-x-6 text-white text-center items-center">
-              <li>
-                <Image width={70} height={20} src={"/apedul.png"} alt={""} />
-              </li>
-              <li>
-                <p className="text-black font-bold">Apedul</p>
-              </li>
-              <li>
+            <div className="flex justify-center items-center gap-4">
+              <Image width={70} height={20} src={"/apedul.png"} alt={""} />
+              <p className="text-black font-bold">Apedul</p>
+            </div>
+            <div className="flex justify-center items-center gap-4 mr-4">
+              <Link href="/guess">
+                <p className="text-black font-bold">Guess</p>
+              </Link>
                 <p className="text-black text-3xl">|</p>
-              </li>
-              <li className="">
-                <Link href="/guess">
-                  <p className="text-black font-bold">Guess</p>
-                </Link>
-              </li>
-              <li>
-                <p className="text-black text-3xl">|</p>
-              </li>
-              {/* <li> */}
-                <Button text={"Support Us"} style={"rounded-md p-2"} onClick={() => {}}/>
-              {/* </li> */}
-            </ul>
+              <Button
+                text={"Support Us"}
+                style={"text-white rounded-md p-2"}
+                onClick={() => {}}
+              />
+            </div>
           </div>
         </div>
       </div>
